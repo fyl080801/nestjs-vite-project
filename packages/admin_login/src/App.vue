fbcdea3cbfc8eb89155cfcc825200262069185b4
fyl080801 <fyl080801@hotmail.com>
2021-04-21 23:32:19 +0800

    chore: new module

---
+++ packages/admin_login/src/App.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { ConfigProvider } from 'ant-design-vue';
+</script>
+
+<template>
+  <ConfigProvider>
+    <RouterView />
+  </ConfigProvider>
+</template>
--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppModule } from '@seed/app';
 import { ViteServiceModule } from '@seed/vite_service';
+import { AdminLoginModule } from '@seed/admin_login';
 
 @Module({
-  imports: [AppModule, ViteServiceModule],
+  imports: [AppModule, ViteServiceModule, AdminLoginModule],
   controllers: [],
   providers: [],
 })
