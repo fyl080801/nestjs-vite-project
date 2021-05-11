bb557a0e2577c58d7ef01394266294bd814e05f6
fengyuanliang3 <fengyuanliang2@jd.com>
2021-05-11 14:21:50 +0800

    feat: profile

--- packages/app/src/router/index.ts
+++ packages/app/src/router/index.ts
@@ -33,6 +33,22 @@ export const constantRoutes: RouteRecordRaw[] = [
       },
     ],
   },
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    meta: {
+      hidden: true,
+    },
+    children: [
+      {
+        path: 'index',
+        component: () => import('../views/profile/index.vue'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true },
+      },
+    ],
+  },
 ];
 
 export const asyncRoutes: RouteRecordRaw[] = [];
