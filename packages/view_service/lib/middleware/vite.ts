a6cb7d7828b187a77dec359433768faa9ffd29bc
fengyuanliang3 <fengyuanliang2@jd.com>
2021-06-16 20:58:48 +0800

    chore: package

--- packages/view_service/lib/middleware/vite.ts
+++ packages/view_service/lib/middleware/vite.ts
@@ -1,13 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
 import { ENV, Environments } from '@nestseed/common';
+import { IncomingMessage, ServerResponse } from 'http';
 import { ViewService } from '../service/view';
 
 @Injectable()
 export class ViteMiddleware implements NestMiddleware {
   constructor(private readonly service: ViewService) {}
 
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(req: IncomingMessage, res: ServerResponse, next: Function) {
     if (ENV === Environments.development) {
       (await this.service.bootstrap()).middlewares(req, res, next);
     } else {
